77a7bc68a8bacdf52eed899ec260e19bc70e96fe
alejo_jimenez <alejandro.jimenez@tekus.co>
2024-01-18 19:31:29 -0500

    Add sign-up class

--- cypress/e2e/tests/place-order.cy.js
+++ cypress/e2e/tests/place-order.cy.js
@@ -1,7 +1,11 @@
 /// <reference types="cypress" />
 
+import { CommonPageHelper } from "../pages/common-page/common-page.helper"
+
 describe('place order',()=>{
     it('Place order with newly created user account',()=>{
-        
+        CommonPageHelper.navigateToTheApplication();
+        CommonPageHelper.clickOnSignUpOption();
+        cy.wait(2000);
     })
 })
